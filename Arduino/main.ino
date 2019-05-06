7a6998da661084c2a4cb905d1edea2049034c52a
Trenton Rochelle <trenton_rochelle@ucsb.edu>
2019-05-05 17:16:45 -0700

    first commit

---
+++ Arduino/main.ino
@@ -0,0 +1,108 @@
+//  Sketc: basicSerialWithNL_001
+// 
+//  Uses hardware serial to talk to the host computer and software serial 
+//  for communication with the Bluetooth module
+//  Intended for Bluetooth devices that require line end characters "\r\n"
+//
+//  Pins
+//  Arduino 5V out TO BT VCC
+//  Arduino GND to BT GND
+//  Arduino D9 to BT RX through a voltage divider
+//  Arduino D8 BT TX (no need voltage divider)
+//
+//  When a command is entered in the serial monitor on the computer 
+//  the Arduino will relay it to the bluetooth module and display the result.
+//
+ 
+#include <Servo.h>
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(8, 9); // RX | TX
+Servo servoWaist;  // create servo object to control a servo
+Servo servoShoulder;
+Servo servoElbow;
+int num;
+const long baudRate = 9600; 
+char c=' ';
+String input = "";
+boolean NL = true;
+void setup() 
+{
+    servoElbow.attach(2);
+    servoShoulder.attach(3);
+    servoElbow.writeMicroseconds(1000);
+    servoShoulder.writeMicroseconds(1000);
+    randomSeed(analogRead(0));
+
+//    Serial.begin(baudRate);
+//    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+//    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+//    Serial.println(" ");
+ 
+    BTserial.begin(baudRate);  
+//    Serial.print("BTserial started at "); Serial.println(baudRate);
+//    Serial.println(" ");
+}
+
+String getString_BT(){
+    String in = "";
+    while(!BTserial.available()){
+      
+    }
+//    servoElbow.detach();
+    c = BTserial.read();
+    in = in + c;
+    while(BTserial.available() | BTserial.available()){
+      c = BTserial.read();
+      in = in + c;
+    }
+//    Serial.println(in);
+//    servoElbow.attach(11);
+
+  return in;
+}
+
+void loop()
+{
+    // Read from the Bluetooth module and send to the Arduino Serial Monitor
+    input = getString_BT();
+    Serial.println(input);
+    char ch = input[0];
+    ch = ch & ~(0x20);
+    input.remove(0,1);
+    int n = input.toInt();  //convert readString into a number
+//    Serial.println(n); //so you can see the integer
+    switch(ch){
+//      case('W'):{
+//        servoWaist.writeMicroseconds(n);
+//        break;
+//      }
+      case('S'):{
+        servoShoulder.writeMicroseconds(n);
+        break;
+      }
+      case('E'):{
+        servoElbow.writeMicroseconds(n);
+        break;
+      }
+      case(DEFAULT):{
+        break;
+      }
+
+      
+    }
+
+ 
+ 
+//    // Read from the Serial Monitor and send to the Bluetooth module
+//    if (Serial.available())
+//    {
+//        c = Serial.read();
+//        BTserial.write(c);   
+// 
+//        // Echo the user input to the main window. The ">" character indicates the user entered text.
+//        if (NL) { Serial.print(">");  NL = false; }
+//        Serial.write(c);
+//        if (c==10) { NL = true; }
+//    }
+ 
+}
---
+++ Bluetooth-Terminal-master/app/src/main/java/me/aflak/bluetoothterminal/Chat.java
@@ -0,0 +1,264 @@
+package me.aflak.bluetoothterminal;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.concurrent.TimeUnit;
+
+import me.aflak.bluetooth.Bluetooth;
+
+public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCallback {
+    private String name;
+    private Bluetooth b;
+    private EditText message;
+    private Button send;
+    private TextView text;
+    private ScrollView scrollView;
+    private static SeekBar seek_bar;
+    private boolean registered=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView)findViewById(R.id.text);
+        message = (EditText)findViewById(R.id.message);
+        send = (Button)findViewById(R.id.send);
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+
+        text.setMovementMethod(new ScrollingMovementMethod());
+        send.setEnabled(false);
+
+        b = new Bluetooth(this);
+        b.enableBluetooth();
+
+        b.setCommunicationCallback(this);
+
+        int pos = getIntent().getExtras().getInt("pos");
+        name = b.getPairedDevices().get(pos).getName();
+
+        Display("Connecting...");
+        b.connectToDevice(b.getPairedDevices().get(pos));
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = message.getText().toString();
+                message.setText("");
+                b.send(msg);
+                Display("You: "+msg);
+            }
+        });
+
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mReceiver, filter);
+        registered=true;
+
+        seek_bar = (SeekBar)findViewById(R.id.seekBar);
+
+        seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progress_value;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress_value = progress;
+                progress_value = 600 + (progress_value*14);
+                b.send("e"+String.valueOf(progress_value));
+                try{
+                    TimeUnit.MILLISECONDS.sleep(100);
+                }
+                catch(InterruptedException e) {
+                    System.out.println("got interrupted!");
+                }
+//                Toast.makeText(Chat.this,"SeekBar value is: " + String.valueOf(progress_value),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+//                    Toast.makeText(Chat.this,"SeekBar Started Tracking",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+//                    Toast.makeText(Chat.this,"SeekBar Stopped Tracking",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(registered) {
+            unregisterReceiver(mReceiver);
+            registered=false;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.close:
+                b.removeCommunicationCallback();
+                b.disconnect();
+                Intent intent = new Intent(this, Select.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            case R.id.rate:
+                Uri uri = Uri.parse("market://details?id=" + this.getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + this.getPackageName())));
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void Display(final String s){
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.append(s + "\n");
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+    @Override
+    public void onConnect(BluetoothDevice device) {
+        Display("Connected to "+device.getName()+" - "+device.getAddress());
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                send.setEnabled(true);
+            }
+        });
+    }
+
+    @Override
+    public void onDisconnect(BluetoothDevice device, String message) {
+        Display("Disconnected!");
+        Display("Connecting again...");
+        b.connectToDevice(device);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        Display(name+": "+message);
+    }
+
+    @Override
+    public void onError(String message) {
+        Display("Error: "+message);
+    }
+
+    @Override
+    public void onConnectError(final BluetoothDevice device, String message) {
+        Display("Error: "+message);
+        Display("Trying again in 3 sec.");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        b.connectToDevice(device);
+                    }
+                }, 2000);
+            }
+        });
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                Intent intent1 = new Intent(Chat.this, Select.class);
+
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered=false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        if(registered) {
+                            unregisterReceiver(mReceiver);
+                            registered=false;
+                        }
+                        startActivity(intent1);
+                        finish();
+                        break;
+                }
+            }
+        }
+    };
+//    public void seekbarr(Bluetooth b){
+//        seek_bar = (SeekBar)findViewById(R.id.seekBar);
+//
+//        seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//
+//                int progress_value;
+//                @Override
+//                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                    progress_value = progress;
+//                    progress_value = 600 + (progress_value*14);
+//                    b.send("e"+String.valueOf(progress_value));
+//                    Toast.makeText(Chat.this,"SeekBar value is: " + String.valueOf(progress_value),Toast.LENGTH_LONG).show();
+//                }
+//
+//                @Override
+//                public void onStartTrackingTouch(SeekBar seekBar) {
+////                    Toast.makeText(Chat.this,"SeekBar Started Tracking",Toast.LENGTH_LONG).show();
+//                }
+//
+//                @Override
+//                public void onStopTrackingTouch(SeekBar seekBar) {
+////                    Toast.makeText(Chat.this,"SeekBar Stopped Tracking",Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        );
+//
+//    }
+}
